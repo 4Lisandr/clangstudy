8b22bfcc15d16375add62c65d6f5951eed095058
Alex <sumy.ua@gmail.com>
2021-08-31 12:42:07 +0300

    pointers turtle *t

--- turtle.c
+++ turtle.c
@@ -6,16 +6,16 @@ typedef struct{
 	int age;
 } turtle;
 
-void happy_birthday(turtle t){
-	t.age = t.age + 1;
+void happy_birthday(turtle *t){
+	(*t).age++;
 	printf("Happy birthday, %s! Now you are %i years old!\n",
-	t.name, t.age);
+	(*t).name, (*t).age);
 }
 
 int main(){
 	/* code */
 	turtle myrtle = {"Tortilla", "Leatherback Turtle", 99};
-	happy_birthday(myrtle);
+	happy_birthday(&myrtle);
 	printf("%s is %i\n", myrtle.name, myrtle.age);
 	return 0;
 }
