13db5559d4a86a236a08dded2e8937c7c09e51cd
Alex <sumy.ua@gmail.com>
2021-08-31 12:23:46 +0300

    method: void happy_birthday(turtle t)

--- turtle.c
+++ turtle.c
@@ -1,7 +1,21 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct{
 	const char *name;
 	const char *species;
 	int age;
 } turtle;
+
+void happy_birthday(turtle t){
+	t.age = t.age + 1;
+	printf("Happy birthday, %s! Now you are %i years old!\n",
+	t.name, t.age);
+}
+
+int main(){
+	/* code */
+	turtle myrtle = {"Tortilla", "Leatherback Turtle", 99};
+	happy_birthday(myrtle);
+	printf("%s is %i\n", myrtle.name, myrtle.age);
+	return 0;
+}
