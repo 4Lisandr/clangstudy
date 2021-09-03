64fde13dc530080bc2d21729754a206552a655fe
Alex <sumy.ua@gmail.com>
2021-09-04 00:07:35 +0300

    match

--- find.c
+++ find.c
@@ -2,27 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-void find(){
+int sports_no_bieber(char *s){
+	return strstr(s, "sport") && !strstr(s, "Bieber");
+}
+
+int sports_or_workout(char *s){
+	return strstr(s, "sport") || strstr(s, "workout");
+}
+
+int arts_theater(char *s){
+	return strstr(s, "arts") && strstr(s, "theater");
+}
+
+void find(int (*match)(char*)){
 	int NUM_ADS = 5;
 	char *ADS[] = {
-		"William: single, sport, TV",
-		"Luis: single, books, food",
-		"Michel: divirced, sport, Biber",
-		"Piter: single, chess",
-		"John: single, sport, TV"
+		"William: single, sport, TV, theater",
+		"Luis: single, books, food, arts",
+		"Michel: divirced, sport, Bieber",
+		"Piter: single, chess, workout",
+		"John: single, sport, TV, theater, arts"
 	};
 	int i;
 	puts("Results of search:");
 	puts("-----------");
 	for(i=0; i<NUM_ADS; i++){
-		if(strstr(ADS[i],"sport") &&
-		!strstr(ADS[i], "Biber")){
+		if(match(ADS[i])){
 			printf("%s\n", ADS[i]);
 		}
 	}
 	puts("-------------");
 }
 int main(){
-	find();
+	find(sports_no_bieber);
+	find(sports_or_workout);
+	find(arts_theater);
 	return 0;
 }
