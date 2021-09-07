e885b4a097104fb58c35ec66429b37381adcb603
Alex <sumy.ua@gmail.com>
2021-09-08 01:54:56 +0300

    response.c

---
+++ response.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum response_type{DUMP, SECOND_CHANCE, MARRIAGE};
+typedef struct {
+	char* name;
+	enum response_type type;
+} response;
+
+void dump(response r){
+	printf("Dear %s\n", r.name);
+	puts("Bad news: ........");
+}
+
+void second_chance(response r){
+	printf("Dear %s\n", r.name);
+	puts("Good news: ......");
+}
+
+void marriage(response r){
+	printf("Dear %s\n", r.name);
+	puts("Congratulations! .......");
+}
+
+int main(){
+	response r[] = {
+		{"Micke", DUMP}, {"Luis", SECOND_CHANCE},
+		{"Matt", SECOND_CHANCE}, {"Bill", MARRIAGE}
+	};
+	for (int i = 0; i < 4; ++i){
+		switch(r[i].type){
+		case DUMP:
+			dump(r[i]);
+			break;
+		case SECOND_CHANCE:
+			second_chance(r[i]);
+			break;
+		default:
+			marriage(r[i]);
+		}
+	}
+	return 0;
+}
